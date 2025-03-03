e1c2c761e7c21155398bd8c2921eb2499762d810
valensoto486 <valensoto486@gmail.com>
2025-03-02 23:01:00 -0500

    fix: Cambio estilo del titulo

--- src/pages/Diseases.jsx
+++ src/pages/Diseases.jsx
@@ -11,66 +11,90 @@ const Diseases = () => {
 
   const diseases = {
     "Bajo peso": [
-      { title: "Anemia", 
-        content: "Déficit de hierro en la sangre, causando fatiga y debilidad.", 
-        prevention: "Consumir alimentos ricos en hierro como carnes magras, espinacas, legumbres y cereales fortificados." 
+      {
+        title: "Anemia",
+        content: "Déficit de hierro en la sangre, causando fatiga y debilidad.",
+        prevention:
+          "Consumir alimentos ricos en hierro como carnes magras, espinacas, legumbres y cereales fortificados.",
       },
-      { title: "Osteoporosis", 
-        content: "Huesos frágiles debido a la falta de calcio y vitamina D.", 
-        prevention: "Consumir productos lácteos, almendras, vegetales de hoja verde y tomar suficiente sol para la producción de vitamina D." 
+      {
+        title: "Osteoporosis",
+        content: "Huesos frágiles debido a la falta de calcio y vitamina D.",
+        prevention:
+          "Consumir productos lácteos, almendras, vegetales de hoja verde y tomar suficiente sol para la producción de vitamina D.",
       },
-      { title: "Deficiencias Nutricionales", 
-        content: "Falta de vitaminas esenciales para el cuerpo.", 
-        prevention: "Mantener una dieta equilibrada con frutas, verduras, proteínas y carbohidratos saludables." 
+      {
+        title: "Deficiencias Nutricionales",
+        content: "Falta de vitaminas esenciales para el cuerpo.",
+        prevention: "Mantener una dieta equilibrada con frutas, verduras, proteínas y carbohidratos saludables.",
       },
-      { title: "Problemas Cardiacos", 
-        content: "Los problemas cardiacos como el prolapso de la valvula mitrial, las arritmias y la insuficiencia cardíaca pueden surgir a partir de un IMC bajo. Además, un desequilibrio de los minerales necesarios para la función muscular puede alterar el ritmo normal del corazón", 
-        prevention: "No fumar\n Realizar más actividades fisicas\n Llevar una dieta saludable para el corazón con un consumo alto de verduras y frutas" 
-      },
-    ],
-    "Saludable": [
-      { title: "Menor riesgo de enfermedades", 
-        content: "Mantiene un equilibrio adecuado de nutrientes y bienestar general.", 
-        prevention: "Seguir un estilo de vida activo, mantener una dieta balanceada y realizar chequeos médicos regulares." 
-      },
-    ],
-    "Sobrepeso": [
-      { title: "Hipertensión", 
-        content: "Aumento de la presión arterial con riesgo de enfermedades cardíacas.", 
-        prevention: "Reducir el consumo de sodio, realizar actividad física regularmente y mantener un peso saludable." 
-      },
-      { title: "Resistencia a la insulina", 
-        content: "Mayor probabilidad de desarrollar diabetes tipo 2.", 
-        prevention: "Consumir alimentos con bajo índice glucémico, evitar azúcares refinados y mantener un estilo de vida activo." 
-      },
-      { title: "Dolores articulares", 
-        content: "Sobrecarga en las articulaciones causando molestias y desgaste.", 
-        prevention: "Mantener un peso adecuado, realizar ejercicios de bajo impacto y fortalecer los músculos de soporte." 
-      },
-      { title: "Apnea del sueño", 
-        content: "Problemas en la respiración, la respiración es superficial y se detiene durante el sueño por la cantidad excesiva de grasa que se almacena al rededor del cuello y bloquea las vías respiratorias.", 
-        prevention: "Perder grasa corporal\n Dormir de lado\n Realizar más actividades fisicas\n Mantener una alimentación equilibrada\n Evitar fumar" 
+      {
+        title: "Problemas Cardiacos",
+        content:
+          "Los problemas cardiacos como el prolapso de la valvula mitrial, las arritmias y la insuficiencia cardíaca pueden surgir a partir de un IMC bajo. Además, un desequilibrio de los minerales necesarios para la función muscular puede alterar el ritmo normal del corazón",
+        prevention:
+          "No fumar. Realizar más actividades fisicas. Llevar una dieta saludable para el corazón con un consumo alto de verduras y frutas.",
       },
     ],
-    "Obesidad": [
-      { title: "Diabetes tipo 2", 
-        content: "Alteración en la regulación del azúcar en sangre.", 
-        prevention: "Controlar el consumo de carbohidratos simples, hacer ejercicio regularmente y mantener un peso saludable." 
-      },
-      { title: "Enfermedades cardíacas", 
-        content: "Aumento del riesgo de infartos y otros problemas cardiovasculares.", 
-        prevention: "Llevar una dieta baja en grasas saturadas, hacer ejercicio y controlar el estrés." 
-      },
-      { title: "Apnea del sueño", 
-        content: "Dificultades respiratorias al dormir debido al exceso de peso.", 
-        prevention: "Bajar de peso, evitar el alcohol y dormir en posiciones adecuadas." 
-      },
-      { title: "Cancer de colon ", 
-        content: "Vesícula biliar y cáncer de endometrio son algunos de los diferentes tipos de cáncer que a menudo se asocian con peso corporal excesivo y alto IMC.", 
-        prevention: "Bajar de peso, evitar el alcohol y dormir en posiciones adecuadas." 
+    Saludable: [
+      {
+        title: "Menor riesgo de enfermedades",
+        content: "Mantiene un equilibrio adecuado de nutrientes y bienestar general.",
+        prevention:
+          "Seguir un estilo de vida activo, mantener una dieta balanceada y realizar chequeos médicos regulares.",
       },
     ],
-  };
+    Sobrepeso: [
+      {
+        title: "Hipertensión",
+        content: "Aumento de la presión arterial con riesgo de enfermedades cardíacas.",
+        prevention: "Reducir el consumo de sodio, realizar actividad física regularmente y mantener un peso saludable.",
+      },
+      {
+        title: "Resistencia a la insulina",
+        content: "Mayor probabilidad de desarrollar diabetes tipo 2.",
+        prevention:
+          "Consumir alimentos con bajo índice glucémico, evitar azúcares refinados y mantener un estilo de vida activo.",
+      },
+      {
+        title: "Dolores articulares",
+        content: "Sobrecarga en las articulaciones causando molestias y desgaste.",
+        prevention:
+          "Mantener un peso adecuado, realizar ejercicios de bajo impacto y fortalecer los músculos de soporte.",
+      },
+      {
+        title: "Apnea del sueño",
+        content:
+          "Problemas en la respiración, la respiración es superficial y se detiene durante el sueño por la cantidad excesiva de grasa que se almacena al rededor del cuello y bloquea las vías respiratorias.",
+        prevention:
+          "Perder grasa corporal. Dormir de lado. Realizar más actividades fisicas. Mantener una alimentación equilibrada. Evitar fumar.",
+      },
+    ],
+    Obesidad: [
+      {
+        title: "Diabetes tipo 2",
+        content: "Alteración en la regulación del azúcar en sangre.",
+        prevention:
+          "Controlar el consumo de carbohidratos simples, hacer ejercicio regularmente y mantener un peso saludable.",
+      },
+      {
+        title: "Enfermedades cardíacas",
+        content: "Aumento del riesgo de infartos y otros problemas cardiovasculares.",
+        prevention: "Llevar una dieta baja en grasas saturadas, hacer ejercicio y controlar el estrés.",
+      },
+      {
+        title: "Apnea del sueño",
+        content: "Dificultades respiratorias al dormir debido al exceso de peso.",
+        prevention: "Bajar de peso, evitar el alcohol y dormir en posiciones adecuadas.",
+      },
+      {
+        title: "Cancer de colon",
+        content:
+          "Vesícula biliar y cáncer de endometrio son algunos de los diferentes tipos de cáncer que a menudo se asocian con peso corporal excesivo y alto IMC.",
+        prevention: "Bajar de peso, evitar el alcohol y dormir en posiciones adecuadas.",
+      },
+    ],
+  }
 
   useEffect(() => {
     const status = localStorage.getItem("imcStatus");
@@ -81,8 +105,8 @@ const Diseases = () => {
   }, []);
 
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % enfermedades.length);
-  };
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % enfermedades.length)
+  }
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + enfermedades.length) % enfermedades.length);
@@ -95,10 +119,9 @@ const Diseases = () => {
         <AppName />
       </div>
       {isSidebarOpen && <Sidebar onClose={() => setIsSidebarOpen(false)} />}
-
+      <h2 className="text-3xl font-bold mb-8 text-[#36AAFF] text-center">Posibles Enfermedades - {imcStatus}</h2>
       {enfermedades.length > 0 && (
         <div className="w-full max-w-md mx-auto bg-[#F5F9FF] p-4 border-2 border-[#36AAFF] rounded-lg text-center">
-          <h2 className="text-xl font-bold mb-4 text-[#36AAFF]">Posibles Enfermedades</h2>
           <div className="mb-6">
             <h3 className="text-lg font-semibold text-[#36AAFF]">{enfermedades[currentIndex].title}</h3>
             <p className="text-gray-600 mt-2">{enfermedades[currentIndex].content}</p>
